e661defae92686d6980b3e388f5a5a7ff028f701
mandarinx <mandarin@mandarin.no>
2017-10-26 08:43:18 +0200

    Load ScriptableObject on pre-build

---
+++ Assets/Scripts/Editor/BuildHooks.cs
@@ -0,0 +1,19 @@
+﻿using HyperGames.AssetBundles;
+using UnityEditor;
+using UnityEngine;
+
+public class BuildHooks {
+    
+    [MenuItem("Dev/PreBuild")]
+    public static void PreBuild() {
+        LoadSO<AssetBundleManagerConfig>("Assets/Configs/AssetBundleManagerConfig.asset");
+    }
+
+    private static void LoadSO<T>(string path) where T : ScriptableObject {
+        Debug.Log("[SOTEST] " + 
+                  (AssetDatabase.LoadAssetAtPath<T>(path) != null 
+                      ? "Could" 
+                      : "Couldn't") + 
+                  " load " + path);
+    }
+}
